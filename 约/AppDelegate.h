67deb0cc88687657a82dc3479e23db1b8e33df80
boolean93 <boolean93@163.com>
2015-06-05 11:40:26 +0800

    Initial Commit

---
+++ 约/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  约
+//
+//  Created by BooleanMac on 15/6/5.
+//  Copyright (c) 2015年 Boolean93. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 约/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  约
+//
+//  Created by BooleanMac on 15/6/5.
+//  Copyright (c) 2015年 Boolean93. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
